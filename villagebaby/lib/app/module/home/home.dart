5d22bbc7c6208a843c60bd4eee18c4ab61b527c8
sangwoo <tjrtkddnaos@naver.com>
2023-02-28 14:49:03 +0900

    Feat: 증감 버튼 action 추가

--- villagebaby/lib/app/module/home/home.dart
+++ villagebaby/lib/app/module/home/home.dart
@@ -1,40 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:villagebaby/app/global/components/counting_button.dart';
+import 'package:villagebaby/app/module/home/home_cubit.dart';
+import 'package:villagebaby/app/module/home/home_state.dart';
+import 'package:villagebaby/domain/repository/counting_repository.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          const Spacer(flex: 2),
-          Text(
-            '1',
-            style: TextStyle(
-              fontSize: 80,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 50),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CountingButton(
-                icon: const Icon(Icons.add),
-                onPressed: () {},
-              ),
-              const SizedBox(width: 100),
-              CountingButton(
-                icon: const Icon(Icons.remove),
-                onPressed: () {},
-              ),
-            ],
-          ),
-          const Spacer(flex: 3),
-        ],
+    return BlocProvider(
+      create: (context) => HomeCubit(
+        countingRepository: context.read<CountingRepository>(),
       ),
+      child: Scaffold(
+        body: _body(),
+      ),
+    );
+  }
+
+  Widget _body() {
+    return BlocBuilder<HomeCubit, HomeState>(
+      builder: (context, state) {
+        return Column(
+          children: [
+            const Spacer(flex: 2),
+            Text(
+              '${state.count}',
+              style: const TextStyle(
+                fontSize: 80,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CountingButton(
+                  icon: const Icon(Icons.remove),
+                  onPressed: () => context.read<HomeCubit>().decreaseCount(),
+                ),
+                const SizedBox(width: 100),
+                CountingButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () => context.read<HomeCubit>().increaseCount(),
+                ),
+              ],
+            ),
+            const Spacer(flex: 3),
+          ],
+        );
+      },
     );
   }
 }
