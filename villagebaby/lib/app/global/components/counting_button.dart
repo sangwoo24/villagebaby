f581caa3f30a760a70674dc965096049a0141120
sangwoo <tjrtkddnaos@naver.com>
2023-02-28 14:48:47 +0900

    Fix: counting_button onPressed 누락코드 추가

--- villagebaby/lib/app/global/components/counting_button.dart
+++ villagebaby/lib/app/global/components/counting_button.dart
@@ -12,14 +12,17 @@ class CountingButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 60,
-      height: 60,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: const Color(0xFFEFEFEF), width: 2),
+    return GestureDetector(
+      onTap: () => onPressed(),
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: const Color(0xFFEFEFEF), width: 2),
+        ),
+        child: icon,
       ),
-      child: icon,
     );
   }
 }
