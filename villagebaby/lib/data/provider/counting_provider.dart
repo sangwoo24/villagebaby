bb89310a714d26ca28d884db0292af74964d8565
sangwoo <tjrtkddnaos@naver.com>
2023-02-28 14:31:00 +0900

    Feat: 카운팅 Data Layer 작성

---
+++ villagebaby/lib/data/provider/counting_provider.dart
@@ -0,0 +1,13 @@
+class CountingProvider {
+  Future<int> increaseCount({required int count}) async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    return count + 1;
+  }
+
+  Future<int> decreaseCount({required int count}) async {
+    await Future.delayed(const Duration(seconds: 1));
+
+    return count - 1;
+  }
+}
