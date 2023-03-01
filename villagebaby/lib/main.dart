4dc5cc745200c4e37634705b89e972ec8e0aad44
sangwoo <tjrtkddnaos@naver.com>
2023-03-01 18:46:59 +0900

    Feat: Hive Init

--- villagebaby/lib/main.dart
+++ villagebaby/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:villagebaby/app/app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
   runApp(const App());
 }
